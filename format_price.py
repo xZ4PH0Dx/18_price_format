acae41b730c096e04fcee4ef626945b78335a844
Z4PH0D <dreamfall3r@gmail.com>
2018-05-14 15:13:52 +0200

    First version

--- format_price.py
+++ format_price.py
@@ -1,5 +1,22 @@
-def format_price(price):
-    pass
+import argparse
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('number', help='input number', type=float)
+    return parser.parse_args()
+
+
+def format_price(price=None):
+    if not isinstance(price, float):
+        return None
+    if price == int(price):
+        return '{:,.2f}'.format(price).replace('.00', '').replace(',', ' ')
+    else:
+        return '{:,.2f}'.format(price).replace(',', ' ')
+
 
 if __name__ == '__main__':
-    pass
+    args = create_parser()
+
+    print(format_price(args.number))
--- tests.py
+++ tests.py
@@ -1,1 +1,22 @@
-# TODO place your tests here
+import unittest
+from format_price import format_price
+
+
+class FormatPriceTestCase(unittest.TestCase):
+    def test_none_price(self):
+        self.assertIsNone(format_price())
+
+    def test_minus_price(self):
+        self.assertEqual('-4', format_price(-4.0))
+        self.assertEqual('-5.50', format_price(-5.5000))
+
+    def test_zero_num_price(self):
+        self.assertEqual('0.50', format_price(.5))
+        self.assertEqual('0.10', format_price(.099))
+
+    def test_str_price(self):
+        self.assertEqual(None, format_price('qwe'))
+
+
+if __name__ == '__main__':
+    unittest.main()
